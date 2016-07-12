33f604b5592b1a8efc77c845021faabc302c0398
me222wm <me222wm@student.lnu.se>
2016-07-12 10:31:06 +0200

    Removed rest db from deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,7 +65,6 @@ namespace :deploy do
     on primary fetch(:migration_role) do
       within release_path do
         with rails_env: fetch(:rails_env) do
-          execute :rake, "db:reset"
           execute :rake, "db:seed"
         end
       end
